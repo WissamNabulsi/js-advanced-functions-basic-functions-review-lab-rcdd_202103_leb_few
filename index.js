9e03113c2f40cc92ab9eb63c2e498c858f23c9ef
Wissam Nabulsi <wissamnabulsi75@gmail.com>
2021-04-27 09:40:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function saturdayFun(activity="roller-skate") {
   return `This Saturday, I want to ${activity}!`
 }
 
-let mondayWork = function(work ="go to the office") {
-  return 'This Monday, I will ${work}.'
+let mondayWork = function(activity="go to the office") {
+  return `This Monday, I will ${activity}.`
 }
